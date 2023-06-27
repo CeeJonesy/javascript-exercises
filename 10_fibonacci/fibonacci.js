9cbfa7773a710b54dac4e56cceef4d97ea28565e
Chris <116323670+CeeJonesy@users.noreply.github.com>
2023-06-27 13:25:36 +0100

    number 10 completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) return 'OOPS';
+    let array = [0,1];
+    for (let i = 2; i < n + 1; i++) {
+        array.push(array[i - 2] + array[i - 1])
+    }
+    return array[n];
 };
 
 // Do not edit below this line
